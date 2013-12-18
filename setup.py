25f0df59f039342cae021ac101e068c3ee78bc0d
mark.shao <37647536@qq.com>
2013-12-18 23:48:54 +0800

    add setup.py script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pagrant',
+    version='1.0',
+    url='https://github.com/markshao/pagrant',
+    license='MIT',
+    author='markshao',
+    description=''
+)
