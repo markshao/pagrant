8052fcb28882c98e0086e3e27c675b7bcf67341f
mark.shao <37647536@qq.com>
2013-12-20 00:02:20 +0800

    add Command as the base class for the sub command

--- pagrant/basecommand.py
+++ pagrant/basecommand.py
@@ -3,6 +3,34 @@
 
 __author__ = ['markshao']
 
+from pagrant import cmdoptions
+from pagrant.cmdparser import ConfigOptionParser
+from pagrant.util import get_prog
+
+__all__ = ['Command']
+
 
 class Command(object):
-    pass
+    name = None
+    usage = None
+    hide = None
+
+    def __init__(self):
+        parser_kw = {
+            'usage': self.usage,
+            'prog': '%s %s' % (get_prog(), self.name),
+            'formatter': UpdatingDefaultsHelpFormatter(),
+            'add_help_option': False,
+            'name': self.name,
+            'description': self.__doc__,
+        }
+
+        self.parser = ConfigOptionParser(**parser_kw)
+
+        # Commands should add options to this option group
+        optgroup_name = '%s Options' % self.name.capitalize()
+        self.cmd_opts = optparse.OptionGroup(self.parser, optgroup_name)
+
+        # Add the general options
+        gen_opts = cmdoptions.make_option_group(cmdoptions.general_group, self.parser)
+        self.parser.add_option_group(gen_opts)
