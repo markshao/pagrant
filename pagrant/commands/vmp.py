289a63830b5a9d03f43174d7e71a2dfeb7a43af2
mark.shao <37647536@qq.com>
2013-12-20 23:15:58 +0800

    add some code

--- pagrant/commands/vmp.py
+++ pagrant/commands/vmp.py
@@ -11,9 +11,14 @@ class VmpCommand(Command):
         The vmp is used to manage the vm providers which
         has been installed in the current pagrant framework
     """
-    name = "vmp"
+    name = "vmprovider"
     usage = """%prog [options]"""
-    summary = "vmp"
+    summary = "vmprovider"
+
+    def __init__(self):
+        # first init the base class
+        super(VmpCommand, self).__init__()
+
 
     def setup_logging(self):
         pass
