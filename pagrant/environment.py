e0fc8c8f4145861ed597d72b2bd9e1dd59a1356d
markshao <37647536@qq.com>
2013-12-22 21:20:24 +0800

    add machine file

---
+++ pagrant/environment.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#coding:utf8
+
+__author__ = ['markshao']
+
+
+# each test contains a environment for test
+
+class Environment(object):
+    pass
---
+++ pagrant/machine.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#coding:utf8
+
+__author__ = ['markshao']
+
+
+class Machine(object):
+    def __init__(self):
+        pass
