368507382cd4af99746507d366875309bd6e1b3c
markshao <37647536@qq.com>
2013-12-23 18:05:39 +0800

    save the code

--- pagrant/commands/init.py
+++ pagrant/commands/init.py
@@ -3,7 +3,10 @@
 
 __author__ = 'markshao'
 
+import os
 from pagrant.basecommand import Command
+
+PAGRANT_CONFIG_FILE_NAME = "Pagrantfile"
 
 
 class InitCommand(Command):
@@ -18,4 +21,13 @@ class InitCommand(Command):
         pass
 
     def run(self, args):
+        pagrant_config_path = os.path.join(os.path.abspath(os.curdir), PAGRANT_CONFIG_FILE_NAME)
+
+        # check the config whether already existed
+        if os.path.exists(pagrant_config_path):
+            need_create_new = raw_input("The Pagrantfile is existed , keep it ? (yes/no)")
+        else:
+            pass
+
+    def _create_new_pagrant_file(self):
         pass
