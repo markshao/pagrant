14447ad8d389ee890aeb2b456d131f2abcfe51cd
markshao <37647536@qq.com>
2013-12-23 23:51:51 +0800

    add fab module

---
+++ pagrant/fab.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#coding:utf8
+
+__author__ = ['markshao']
+
+from fabric.api import settings
+from fabric.operations import run
+
+
+class FabricSupport(object):
+    def __init__(self, host, username, password):
+        self.host = host
+        self.username = username
+        self.password = password
+
+    def execute_shell_command(self, command):
+        with settings(host=self.host, user=self.username, password=self.password):
+            return run(command, shell=True)
+
