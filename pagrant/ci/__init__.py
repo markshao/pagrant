2b043ba7736b4fa3f6838fc2ca66db0484dec68a
markshao <37647536@qq.com>
2014-02-11 22:48:04 +0800

    add support for download the file

--- pagrant/ci/__init__.py
+++ pagrant/ci/__init__.py
@@ -1,6 +1,61 @@
 __author__ = 'root'
 
+import base64
+import subprocess
+import time
+
+import os
+from pagrant.exceptions import PagrantError
+
+
+CONNECTIONS = 10
+TIMEOUT = 5 * 60
+
+
+def get_axel_path():
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), 'downloader', 'axel')
+
 
 class CIBuild(object):
-    def __init__(self, logger):
+    def __init__(self, logger, ci_config):
         self.logger = logger
+        self.ci_config = ci_config
+
+    def get_latest_artifact_url(self):
+        NotImplemented
+
+    def download_to_local(self, destination, need_authentication=False):
+        if os.path.exists(destination):
+            os.remove(destination)
+
+        if need_authentication:
+            b64Val = base64.b64encode("%s:%s" % (self.username, self.password))
+            authentication_header = "Authorization: Basic %s" % b64Val
+            download_command = [get_axel_path(), "-n", str(CONNECTIONS), "-H", "'%s'" % authentication_header, "-o",
+                                destination, self.get_latest_artifact_url()]
+
+        else:
+            download_command = [get_axel_path(), "-n", str(CONNECTIONS), "-o",
+                                destination, self.get_latest_artifact_url()]
+
+        print download_command
+        start_download_time = time.time()
+        try:
+            download_process = subprocess.Popen(download_command)
+        except Exception, e:
+            raise PagrantError("could not start the download proecess")
+
+        while ((time.time() - start_download_time) <= TIMEOUT):
+            status = download_process.poll()
+            if not status and status != 0:
+                time.sleep(5)
+                continue
+            if status != 0:
+                raise PagrantError("download fail")
+            else:
+                break
+        else:
+            raise PagrantError("Timeout")
+
+        if not os.path.exists(destination):
+            raise PagrantError("download fail")
--- pagrant/ci/teamcity/__init__.py
+++ pagrant/ci/teamcity/__init__.py
@@ -7,24 +7,42 @@ from pagrant.exceptions import PagrantError
 
 class TeamCityBuild(CIBuild):
     def __init__(self, logger, ci_config):
-        super(TeamCityBuild, self).__init__(logger)
-        self.tc_host = ci_config.get("host", None)
+        super(TeamCityBuild, self).__init__(logger, ci_config)
+        self.tc_host = self.ci_config.get("host", None)
         assert self.tc_host
-        self.tc_port = ci_config.get("port", None)
+        self.tc_port = self.ci_config.get("port", None)
 
-        self.username = ci_config.get("username", None)
-        self.password = ci_config.get("password", None)
+        self.username = self.ci_config.get("username", None)
+        self.password = self.ci_config.get("password", None)
 
         self.tc = TeamCityRESTApiClient(self.username, self.password, self.tc_host, self.tc_port)
 
-        self.project_name = ci_config.get("project_name", None)
+        self.build_type_name = self.ci_config.get("build_type_name", None)
+        self.artifact_name = self.ci_config.get("artifact_name", None)
 
-    def project_id_by_name(self, name):
+    def project_id_by_name(self, project_name):
         self.tc.get_all_projects()
         for project in self.tc.get_from_server()["project"]:
-            if project["name"] == name:
+            if project["name"] == project_name:
                 return project["id"]
         raise PagrantError("The project is not found")
+
+    def get_build_type_id_by_name(self, build_type_name):
+        self.tc.get_all_build_types()
+        for build_type in self.tc.get_from_server()['buildType']:
+            if build_type['name'] == build_type_name:
+                return build_type['id']
+        raise PagrantError("The build type is not found")
+
+    def get_latest_build_id(self, build_type_id):
+        self.tc.get_all_builds_by_build_type_id(build_type_id)
+        return self.tc.get_from_server()['build'][0]['id']
+
+    def get_latest_artifact_url(self):
+        build_type_id = self.get_build_type_id_by_name(self.build_type_name)
+        latest_build_id = self.get_latest_build_id(build_type_id)
+        return "http://%s:%s/repository/download/%s/%s:id/%s" % (
+            self.tc_host, self.tc_port, build_type_id, latest_build_id, self.artifact_name)
 
 
 if __name__ == "__main__":
@@ -32,7 +50,9 @@ if __name__ == "__main__":
         "host": "shdssinttc1.dctmlabs.com",
         "port": 6080,
         "username": "shaom2",
-        "password": ")Slamdunk1986"
+        "password": ")Slamdunk1986",
+        "build_type_name": "xCPRestServices_Main_Standalone",
+        "artifact_name": "xcp-rest.war"
     })
 
-    print tcb.project_id_by_name("Documentum REST Services")
+    print tcb.download_to_local("/home/mark/Downloads/xcp-rest.war", False)
